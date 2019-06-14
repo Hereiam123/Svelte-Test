48268732ade58265fb46b2fa9efd811f55ce3b13
Brian De Maio <bdemaio1@gmail.com>
2019-06-14 19:28:03 -0400

    Added individual players to score, and created componenet for individual Players

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,21 @@
 <script>
   import Navbar from "./Navbar.svelte";
-  let name = "Hello there!";
-  let points = 100;
-  let showControls = false;
+  import Player from "./Player.svelte";
 
-  const addPoint = () => points++;
-  const removePoint = () => points--;
-  const toggleControls = () => (showControls = !showControls);
+  let players = [{ name: "John", points: 5 }, { name: "Jane", points: 10 }];
 </script>
 
 <style>
-  h1 {
-    color: blue;
-  }
+
 </style>
 
 <Navbar />
 <div class="container">
-  <div class="card">
-    <h1>
-       {name}
-      <button class="btn-sm" on:click={toggleControls}>
-        {#if showControls}-{:else}+{/if}
-      </button>
-    </h1>
-    <h3>Points: {points}</h3>
-    {#if showControls}
-      <button class="btn" on:click={addPoint}>+1</button>
-      <button class="btn btn-dark" on:click={removePoint}>-1</button>
-      <input type="number" bind:value={points} />
-    {/if}
-  </div>
+  {#if players.length === 0}
+    <p>No Players</p>
+  {:else}
+    {#each players as player}
+      <Player name={player.name} points={player.points} />
+    {/each}
+  {/if}
 </div>
---
+++ src/Player.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let name;
+  export let points;
+  let showControls = false;
+
+  const addPoint = () => points++;
+  const removePoint = () => points--;
+  const toggleControls = () => (showControls = !showControls);
+</script>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
+
+<div class="card">
+  <h1>
+     {name}
+    <button class="btn-sm" on:click={toggleControls}>
+      {#if showControls}-{:else}+{/if}
+    </button>
+  </h1>
+  <h3>Points: {points}</h3>
+  {#if showControls}
+    <button class="btn" on:click={addPoint}>+1</button>
+    <button class="btn btn-dark" on:click={removePoint}>-1</button>
+    <input type="number" bind:value={points} />
+  {/if}
+</div>
